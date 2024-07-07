a5c9d2fecb7cd5de5baf8a9d9763cee9359911f5
shashankshampi <shashank.sanket1995@gmail.com>
2024-07-08 00:19:34 +0530

    added test to run sequentially

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,20 +1,24 @@
 use std::error::Error;
 use waka_waka_waku::*;
+use serial_test::serial;
 
 /// Test Suite 1: Basic Node Operation (easy)
 #[tokio::test]
+#[serial]
 async fn node1_test1_verify_node_info() -> Result<(), Box<dyn Error>> {
     verify_node_info().await?;
     Ok(())
 }
 
 #[tokio::test]
+#[serial]
 async fn node1_test2_subscribe_to_topic() -> Result<(), Box<dyn Error>> {
     subscribe_to_topic("/my-app/2/chatroom-1/proto").await?;
     Ok(())
 }
 
 #[tokio::test]
+#[serial]
 async fn node1_test3_publish_message() -> Result<(), Box<dyn Error>> {
     let message = PublishMessage {
         payload: "UmVsYXkgd29ya3MhIQ==".to_string(),
@@ -26,6 +30,7 @@ async fn node1_test3_publish_message() -> Result<(), Box<dyn Error>> {
 }
 
 #[tokio::test]
+#[serial]
 async fn node1_test4_confirm_message_publication() -> Result<(), Box<dyn Error>> {
     let message = PublishMessage {
         payload: "UmVsYXkgd29ya3MhIQ==".to_string(),
@@ -38,18 +43,21 @@ async fn node1_test4_confirm_message_publication() -> Result<(), Box<dyn Error>>
 
 /// Test Suite 2: Inter-Node Communication (advanced)
 #[tokio::test]
+#[serial]
 async fn node2_test1_verify_node2_autoconnection() -> Result<(), Box<dyn Error>> {
     verify_node2_autoconnection().await?;
     Ok(())
 }
 
 #[tokio::test]
+#[serial]
 async fn node2_test2_subscribe_node2_to_topic() -> Result<(), Box<dyn Error>> {
     subscribe_node2_to_topic("/my-app/2/chatroom-1/proto").await?;
     Ok(())
 }
 
 #[tokio::test]
+#[serial]
 async fn node2_test3_publish_message_from_node1_for_node2() -> Result<(), Box<dyn Error>> {
     let message = PublishMessage {
         payload: "UmVsYXkgd29ya3MhIQ==".to_string(),
@@ -61,6 +69,7 @@ async fn node2_test3_publish_message_from_node1_for_node2() -> Result<(), Box<dy
 }
 
 #[tokio::test]
+#[serial]
 async fn node2_test4_confirm_message_publication_from_node2() -> Result<(), Box<dyn Error>> {
     let message = PublishMessage {
         payload: "UmVsYXkgd29ya3MhIQ==".to_string(),
